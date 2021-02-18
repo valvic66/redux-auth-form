451bc67f4a38cbba725f6b1a1f734102dbd9ad17
Micu Valentin <valvic6@gmail.com>
2021-02-18 13:02:30 +0200

    fixed email value on input field

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -5,37 +5,36 @@ import { connect } from 'react-redux';
 
 class UserForm extends Component{
     handleEmailChange = email => {
-        this.props.emailChanged(email);
+        this.props.emailChanged(email.target.value);
     };
 
     render() {
-        console.log(this.props.email);
-
         return (
-        <div className="form-wrapper">
-            <form method="post">
-            <h3 className="form-name">Registration form</h3>
+            <div className="form-wrapper">
+                <form method="post">
+                    <h3 className="form-name">Registration form</h3>
 
-            <div className="user-field">
-                <label htmlFor="email">Email</label>
-                <input
-                type="email"
-                id="email"
-                name="email"
-                required
-                onChange={this.handleEmailChange}
-                // value={this.props.email}
-                />
+                    <div className="user-field">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            autoComplete="false"
+                            type="email"
+                            id="email"
+                            name="email"
+                            required
+                            onChange={this.handleEmailChange}
+                            value={this.props.email}
+                        />
+                    </div>
+
+                    <div className="user-field">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" name="password" required />
+                    </div>
+
+                    <button type="submit">SIGN UP</button>
+                </form>
             </div>
-
-            <div className="user-field">
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" required />
-            </div>
-
-            <button type="submit">SIGN UP</button>
-            </form>
-        </div>
         );
     };
 };
